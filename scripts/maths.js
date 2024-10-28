372ade985993fdb5740d24cf5a8aea9c5da77f9a
carson-37 <carsonhansen37@gmail.com>
2024-10-27 19:47:28 -0600

    Update maths.js

--- scripts/maths.js
+++ scripts/maths.js
@@ -48,7 +48,6 @@ function domLoaded(){
     function math(){
       let n1 = parseFloat(document.getElementById("n1").value);
       let n2 = parseFloat(document.getElementById("n2").value);
-      alert(n1);
       if(operatornum == 1){
         result = add(n1, n2);
       }
